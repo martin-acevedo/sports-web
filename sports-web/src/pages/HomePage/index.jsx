511ce465ec791837c1a730198b050c3f701a1d49
M4rt1nnnn <martinacevedobravo@gmail.com>
2026-05-01 18:43:11 -0400

    cambios iniciales

---
+++ sports-web/src/pages/HomePage/index.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../../components/Navbar"
+// importar dummy
+
+function HomePage(){
+    return <div>
+        <Navbar />
+        <div>
+            {
+                
+            }
+        </div>
+    </div>
+}
+
+export default HomePage
